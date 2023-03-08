d2df626ac6cc6f415dd3fbec69b319d7582246cf
CodeAshen <codeashen@foxmail.com>
2023-03-08 22:20:09 +0800

    chore: organize documents

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,6 +1,7 @@
 /**
  * 提示：如您想使用JS版本的配置文件可参考：https://github.com/xugaoyi/vuepress-theme-vdoing/tree/a2f03e993dd2f2a3afdc57cf72adfc6f1b6b0c32/docs/.vuepress
  */
+import { resolve } from 'path'
 import dayjs from 'dayjs';
 import { VdoingThemeConfig } from 'vuepress-theme-vdoing/types';
 import { defineConfig4CustomTheme, UserPlugins } from 'vuepress/config';
@@ -11,15 +12,16 @@ const DOMAIN_NAME = 'notes.codeashen.top' // 域名 (不带https)
 const WEB_SITE = `https://${DOMAIN_NAME}` // 网址
 
 export default defineConfig4CustomTheme<VdoingThemeConfig>({
-  theme: 'vdoing', // 使用npm主题包
+  theme: "vdoing", // 使用npm主题包
   // theme: resolve(__dirname, '../../vdoing'), // 使用本地主题包
 
   locales: {
-    '/': {
-      lang: 'zh-CN',
+    "/": {
+      lang: "zh-CN",
       title: "CodeAshen's blog",
-      description: '后端技术博客,专注后端技术学习与总结。Java,Python,JavaScript,TypeScript,Spring,css3,html5,Node,git,github等技术文章。',
-    }
+      description:
+        "后端技术博客,专注后端技术学习与总结。Java,Python,JavaScript,TypeScript,Spring,css3,html5,Node,git,github等技术文章。",
+    },
   },
 
   // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
@@ -29,76 +31,76 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
   themeConfig: {
     // 导航配置
     nav: [
-      { text: '首页', link: '/' },
+      { text: "首页", link: "/" },
       {
-        text: 'Spring',
-        link: '/spring/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+        text: "Spring",
+        link: "/spring/", //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
         items: [
           // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
           {
-            text: 'Spring Framework',
+            text: "Spring Framework",
             items: [
-              { text: '《剖析Spring5核心原理》', link: '/note/javascript/' },
-              { text: '《Spring源码轻松学》', link: '/note/js/' },
+              { text: "《剖析Spring5核心原理》", link: "/pages/32011a/" },
+              { text: "《Spring源码轻松学》", link: "/pages/af2fa0/" },
             ],
           },
           {
-            text: 'Spring Boot',
+            text: "Spring Boot",
             items: [
-              { text: 'Spring Boot 2.0深度实践', link: '/note/javascript/' },
+              { text: "Spring Boot 2.0深度实践", link: "/pages/a18a7b/" },
             ],
           },
           {
-            text: 'Spring Cloud',
+            text: "Spring Cloud",
             items: [
-              { text: 'Spring Cloud', link: '/pages/8143cc480faf9a11/' },
-              { text: 'Spring Cloud Alibaba', link: '/pages/8143cc480faf9a11/' },
+              { text: "Spring Cloud", link: "/pages/f31975/" },
+              { text: "Spring Cloud Alibaba", link: "/pages/722430/" },
             ],
           },
         ],
       },
       {
-        text: '中间件',
-        link: '/ui/',
+        text: "中间件",
+        link: "/middleware/",
         items: [
-          { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
-          { text: 'CSS', link: '/pages/0a83b083bdf257cb/' },
+          { text: "RabbitMQ", link: "/pages/630854/" },
+          { text: "RocketMQ", link: "/pages/9b3698/" },
+          { text: "Kafka", link: "/pages/c42700/" },
         ],
       },
       {
-        text: '数据库',
-        link: '/technology/',
+        text: "数据库",
+        link: "/database/",
         items: [
-          { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
-          { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
-          { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
-          { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+          { text: "MySQL8.0详解", link: "/pages/4d1915/" },
+          { text: "Redis从入门到高可用", link: "/pages/726b8d/" },
+          { text: "Elastic Stack", link: "/pages/734bee/" },
         ],
       },
       {
-        text: '内功心法',
-        link: '/technology/',
+        text: "内功心法",
+        link: "/foundation/",
         items: [
-          { text: '操作系统', link: '/pages/9a7ee40fc232253e/' },
-          { text: '计算机网络', link: '/pages/4c778760be26d8b3/' },
-          { text: '数据结构与算法', link: '/pages/117708e0af7f0bd9/' },
+          { text: "操作系统", link: "/os/" },
+          { text: "计算机网络", link: "/network/" },
+          { text: "数据结构与算法", link: "/algorithm/" },
         ],
       },
       {
-        text: '更多',
-        link: '/more/',
+        text: "更多",
+        link: "/more/",
         items: [
-          { text: '云原生', link: '/pages/f2a556/' },
-          { text: 'Devops', link: '/pages/aea6571b7a8bae86/' },
-          { text: '前端', link: '/pages/2d615df9a36a98ed/' },
-          { text: '实用工具', link: '/pages/baaa02/' },
-          { text: '友情链接', link: '/friends/' },
+          { text: "云原生", link: "/cloud-native/" },
+          { text: "Devops", link: "/devops/" },
+          { text: "前端", link: "/frontend/" },
+          { text: "实用工具", link: "/tools/" },
+          { text: "友情链接", link: "/friends/" },
         ],
       },
-      { text: '关于', link: '/about/' },
+      { text: "关于", link: "/about/" },
       {
-        text: '收藏',
-        link: '/pages/beb6c0bd8a66cea6/',
+        text: "收藏",
+        link: "/pages/favorites/",
         // items: [
         //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
         //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
@@ -106,24 +108,25 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         // ],
       },
       {
-        text: '索引',
-        link: '/archives/',
+        text: "索引",
+        link: "/reference/",
         items: [
-          { text: '分类', link: '/categories/' },
-          { text: '标签', link: '/tags/' },
-          { text: '归档', link: '/archives/' },
+          { text: "分类", link: "/categories/" },
+          { text: "标签", link: "/tags/" },
+          { text: "归档", link: "/archives/" },
+          { text: "Reference", link: "/reference/" },
         ],
       },
     ],
-    sidebarDepth: 2,          // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: '/img/logo.png',    // 导航栏logo
-    repo: 'codeashen/notes',  // 导航栏右侧生成Github链接
+    sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+    logo: "/img/logo.png", // 导航栏logo
+    repo: "codeashen/notes", // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
-    lastUpdated: '上次更新',  // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
-    docsDir: 'docs',          // 编辑的文件夹
+    lastUpdated: "上次更新", // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
+    docsDir: "docs", // 编辑的文件夹
     // docsBranch: 'master',  // 编辑的文件所在分支，默认master。 注意：如果你的分支是main则修改为main
-    editLinks: true,          // 启用编辑
-    editLinkText: '编辑',
+    editLinks: true, // 启用编辑
+    editLinkText: "编辑",
 
     //*** 以下是Vdoing主题相关配置，文档：https://doc.xugaoyi.com/pages/a20ce8/ ***//
 
@@ -160,19 +163,20 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     // defaultMode: 'auto',
 
     // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | <自定义>    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
-    sidebar: 'structuring',
+    sidebar: "structuring",
 
     // 文章默认的作者信息，(可在md文件中单独配置此信息) string | {name: string, link?: string}
     author: {
-      name: 'CodeAshen', // 必需
-      link: 'https://github.com/codeashen', // 可选的
+      name: "CodeAshen", // 必需
+      link: "https://github.com/codeashen", // 可选的
     },
 
     // 博主信息 (显示在首页侧边栏)
     blogger: {
-      avatar: 'https://cdn.staticaly.com/gh/codeashen/img-store/master/blog/public/avatar.jpg',
-      name: 'CodeAshen',
-      slogan: '后端界的小学生',
+      avatar:
+        "https://cdn.staticaly.com/gh/codeashen/img-store/master/blog/public/avatar.jpg",
+      name: "CodeAshen",
+      slogan: "后端界的小学生",
     },
 
     // 社交图标 (显示于博主信息栏和页脚栏。内置图标：https://doc.xugaoyi.com/pages/a20ce8/#social)
@@ -180,19 +184,19 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       // iconfontCssFile: '//at.alicdn.com/t/xxx.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自己添加。阿里图片库：https://www.iconfont.cn/
       icons: [
         {
-          iconClass: 'icon-youjian',
-          title: '发邮件',
-          link: 'mailto:codeashen@foxmail.com',
+          iconClass: "icon-youjian",
+          title: "发邮件",
+          link: "mailto:codeashen@foxmail.com",
         },
         {
-          iconClass: 'icon-github',
-          title: 'GitHub',
-          link: 'https://github.com/codeashen',
+          iconClass: "icon-github",
+          title: "GitHub",
+          link: "https://github.com/codeashen",
         },
         {
-          iconClass: 'icon-bilibili',
-          title: 'BiliBili',
-          link: 'https://space.bilibili.com/7403658/',
+          iconClass: "icon-bilibili",
+          title: "BiliBili",
+          link: "https://space.bilibili.com/7403658/",
         },
       ],
     },
@@ -207,9 +211,9 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     // 扩展自动生成frontmatter。（当md文件的frontmatter不存在相应的字段时将自动添加。不会覆盖已有的数据。）
     extendFrontmatter: {
       author: {
-        name: 'CodeAshen',
-        link: 'https://github.com/codeashen'
-      }
+        name: "CodeAshen",
+        link: "https://github.com/codeashen",
+      },
     },
 
     // 自定义hmtl(广告)模块
@@ -218,16 +222,17 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
   // 注入到页面<head>中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
   head: [
-    ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
+    ["link", { rel: "icon", href: "/img/favicon.ico" }], //favicons，资源放在public文件夹
     [
-      'meta',
+      "meta",
       {
-        name: 'keywords',
-        content: '后端博客,个人技术博客,后端,后端开发,后端框架,后端面试题,技术文档,学习,面试,Java,Python,JavaScript,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown',
+        name: "keywords",
+        content:
+          "后端博客,个人技术博客,后端,后端开发,后端框架,后端面试题,技术文档,学习,面试,Java,Python,JavaScript,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown",
       },
     ],
     // ['meta', { name: 'baidu-site-verification', content: '7F55weZDDc' }], // 百度统计的站长验证（你可以去掉）
-    ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+    ["meta", { name: "theme-color", content: "#11a8cd" }], // 移动浏览器主题颜色
     // [
     //   'script',
     //   {
@@ -241,13 +246,10 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
   // 插件配置
   plugins: <UserPlugins>[
     // 网站地图
-    [
-      "sitemap",
-      { hostname: WEB_SITE }
-    ],
+    ["sitemap", { hostname: WEB_SITE }],
 
     // 百度自动推送
-    'vuepress-plugin-baidu-autopush',
+    "vuepress-plugin-baidu-autopush",
 
     // 百度统计
     // [
@@ -260,36 +262,39 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
     // 可以添加第三方搜索链接的搜索框（继承原官方搜索框的配置参数）
     [
-      'thirdparty-search',
+      "thirdparty-search",
       {
         thirdparty: [
           {
-            title: '在MDN中搜索',
-            frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-            behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
+            title: "在MDN中搜索",
+            frontUrl: "https://developer.mozilla.org/zh-CN/search?q=", // 搜索链接的前面部分
+            behindUrl: "", // 搜索链接的后面部分，可选，默认 ''
           },
           {
-            title: '在Runoob中搜索',
-            frontUrl: 'https://www.runoob.com/?s=',
+            title: "在Runoob中搜索",
+            frontUrl: "https://www.runoob.com/?s=",
           },
           {
-            title: '在Bing中搜索',
-            frontUrl: 'https://cn.bing.com/search?q=',
+            title: "在Bing中搜索",
+            frontUrl: "https://cn.bing.com/search?q=",
           },
           {
-            title: '通过百度搜索本站的',
+            title: "通过百度搜索本站的",
             frontUrl: `https://www.baidu.com/s?wd=site%3A${DOMAIN_NAME}%20`,
           },
         ],
-      }
+      },
     ],
 
     // 代码块复制按钮
     [
-      'one-click-copy',
+      "one-click-copy",
       {
-        copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-        copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+        copySelector: [
+          'div[class*="language-"] pre',
+          'div[class*="aside-code"] aside',
+        ], // String or Array
+        copyMessage: "复制成功", // default is 'Copy successfully and then paste it for use.'
         duration: 1000, // prompt message display time.
         showInMobile: false, // whether to display on the mobile side, default: false.
       },
@@ -312,43 +317,42 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
     // 放大图片
     [
-      'vuepress-plugin-zooming',
+      "vuepress-plugin-zooming",
       {
-        selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
+        selector: ".theme-vdoing-content img:not(.no-zoom)", // 排除class是no-zoom的图片
         options: {
-          bgColor: 'rgba(0,0,0,0.6)',
+          bgColor: "rgba(0,0,0,0.6)",
         },
       },
     ],
 
     // 评论
     [
-      'vuepress-plugin-comment',
+      "vuepress-plugin-comment",
       {
-        choosen: 'gitalk',
+        choosen: "gitalk",
         options: {
-          clientID: 'a6e1355287947096b88b',
-          clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-          repo: 'blog-gitalk-comment', // GitHub 仓库
-          owner: 'CodeAshen', // GitHub仓库所有者
-          admin: ['CodeAshen'], // 对仓库有写权限的人
+          clientID: "a6e1355287947096b88b",
+          clientSecret: "f0e77d070fabfcd5af95bebb82b2d574d7248d71",
+          repo: "blog-gitalk-comment", // GitHub 仓库
+          owner: "CodeAshen", // GitHub仓库所有者
+          admin: ["CodeAshen"], // 对仓库有写权限的人
           // distractionFreeMode: true,
-          pagerDirection: 'last', // 'first'正序 | 'last'倒序
-          id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
-          title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
-          labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
-          body:
-            '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
+          pagerDirection: "last", // 'first'正序 | 'last'倒序
+          id: "<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>", //  页面的唯一标识,长度不能超过50
+          title: "「评论」<%- frontmatter.title %>", // GitHub issue 的标题
+          labels: ["Gitalk", "Comment"], // GitHub issue 的标签
+          body: "页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>", // GitHub issue 的内容
         },
       },
     ],
 
     // "上次更新"时间格式
     [
-      '@vuepress/last-updated',
+      "@vuepress/last-updated",
       {
         transformer: (timestamp, lang) => {
-          return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
+          return dayjs(timestamp).format("YYYY/MM/DD, HH:mm:ss");
         },
       },
     ],
@@ -356,12 +360,9 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
   markdown: {
     lineNumbers: true,
-    extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
+    extractHeaders: ["h2", "h3", "h4", "h5", "h6"], // 提取标题到侧边栏的级别，默认['h2', 'h3']
   },
 
   // 监听文件变化并重新构建
-  extraWatchFiles: [
-    '.vuepress/config.ts',
-    '.vuepress/config/htmlModules.ts',
-  ]
-})
+  extraWatchFiles: [".vuepress/config.ts", ".vuepress/config/htmlModules.ts"],
+});
